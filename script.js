78dc9b5c4b5ad3bd2e10303a82a27c07da15d9c6
web-god <rashidiqbal7141+github1@gmail.com>
2023-06-09 13:31:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const pageBox = document.querySelector('.page-box');
+const btnNext = document.querySelector('.btn-next');
+const btnBack = document.querySelector('.btn-back');
+const checkboxPass = document.querySelector('.checkbox-pass');
+const passwordInput = document.querySelector('.password');
+const loginTitle = document.querySelector('.loginTitle-text');
+const userEmail = document.querySelector('.user-email');
+const emailInput = document.querySelector('.email');
+
+btnNext.onclick = (e) => {
+    e.preventDefault();
+    pageBox.classList.add('active-pass');
+    setTimeout(() => passwordInput.focus(), 500);
+    loginTitle.innerHTML = 'Welcome';
+    userEmail.innerHTML = emailInput.value;
+};
+
+btnBack.onclick = (e) => {
+    e.preventDefault();
+    pageBox.classList.remove('active-pass');
+    loginTitle.innerHTML = 'Sign in';
+    userEmail.innerHTML = 'Use your Google Account';
+    emailInput.focus();
+};
+
+checkboxPass.onclick = (e) => {
+    if(checkboxPass.checked) {
+        passwordInput.type= 'text';
+    }
+    else {
+        passwordInput.type= 'password';
+    }
+};
